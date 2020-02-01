5e49ca176632fe5a59c7c65edf646c810dbac534
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2020-02-01 16:29:26 -0500

    last insecure naivete

--- Tsunami.ino
+++ Tsunami.ino
@@ -4,63 +4,107 @@ void PlayerSelect();
 void Player();
 
 State state;
-Timer oceanAmbient;
+Timer oceanAmbientOrClear;
 Timer waveTimer;
+Timer playerSelectedAnim;
 
-Color colors[] = {RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, MAGENTA, WHITE};
+Color colors[] = {WHITE, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, MAGENTA};
 byte playerColor;
 byte waveData;
 byte waveSpeed;
-byte waveColor;
 byte lives;
+bool sending;
+byte waveToSend;
 
 void Ocean() {
-  
+  waveData = 0;
+
   FOREACH_FACE(f) {
-    if (!isValueReceivedOnFaceExpired(f)) {
+    if (!isValueReceivedOnFaceExpired(f) && waveData == 0) { //get the first not 0 face
       waveData = getLastValueReceivedOnFace(f);
-      waveColor = waveData & 7;
-      waveSpeed = waveData & 56;//8 + 16 + 32
-      waveTimer.set(waveSpeed >> 2);
     }
   }
-  setValueSentOnAllFaces(waveData);
-
-  if (!waveTimer.isExpired()) {
+  
+  if (waveData != 0) {
+    byte waveColor = waveData & 7;
+    waveSpeed = waveData & 56;//8 + 16 + 32
+    waveTimer.set(waveSpeed << 2);
     setColor(colors[waveColor]);
-  } else if(oceanAmbient.isExpired()) {
+    waveToSend = waveData;
+  }
+
+  if (waveToSend != 0 && waveTimer.isExpired()) {
+      setValueSentOnAllFaces(waveData);
+      waveToSend = 0;
+  }
+
+  if(waveTimer.isExpired() && oceanAmbientOrClear.isExpired()) { //idle ocean animation
     setColor(OFF);
     setColorOnFace(makeColorRGB(0, 0, 32), random(5));
-    oceanAmbient.set(1000);
+    oceanAmbientOrClear.set(1000);
   }
+
   if (buttonLongPressed()) {
     state = PlayerSelect;
   }
 }
 
 void PlayerSelect() {
-  setColor(RED);
+  setColor(colors[playerColor]);
+  if (buttonSingleClicked()) {
+    //advance color
+    playerColor = (playerColor + 1) % 7 + 1; //unintentional interleave but i am keeping it, i was planning to interleave anyway
+  }
   if (buttonLongPressed()) {
     state = Player;
+    playerSelectedAnim.set(1000);
   }
 }
 
 void Player() {
-  setColor(GREEN);
+  if (!playerSelectedAnim.isExpired()) {
+    setColor(dim(colors[playerColor], sin8_C(playerSelectedAnim.getRemaining() % 255)));
+  } else {
+    setColor(colors[playerColor]);
+  }
   if (buttonLongPressed()) {
     state = Ocean;
+  }
+  if (buttonSingleClicked()) {
+    //catch wave
   }
   if (buttonDoubleClicked()) {
     //send wave
     setValueSentOnAllFaces(waveSpeed + playerColor);
+    sending = true;
+    oceanAmbientOrClear.set(100);
+  }
+  if (sending && oceanAmbientOrClear.isExpired()) {
+    setValueSentOnAllFaces(0);
+    sending = false;
+  }
+  //process incoming wave
+  FOREACH_FACE(f) {
+    if (!isValueReceivedOnFaceExpired(f) && didValueOnFaceChange(f)) {
+      //set wave present timer
+      // if wave not caught decrement life
+      // if catch attempted but no wave present decrement life
+//      waveData = getLastValueReceivedOnFace(f);
+//      if (waveData != 0) {
+//        waveColor = waveData & 7;
+//        waveSpeed = waveData & 56;//8 + 16 + 32
+//        waveTimer.set(waveSpeed >> 2);
+//      }
+    }
   }
 }
 
 void setup() {
   state = Ocean;
-  playerColor = 0;
+  playerColor = 1;
   lives = 6;
   waveSpeed = 56;
+  sending = false;
 }
 
 void loop() {
